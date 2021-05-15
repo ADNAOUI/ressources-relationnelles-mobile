340d201022bbbf6c2e8c5df20f5f47ad0b9b041e
unknown <sullyvan360@gmail.com>
2021-05-15 17:19:03 +0200

    premier commit mon compte

--- src/pages/view/viewProfils/VueAboutProfil.tsx
+++ src/pages/view/viewProfils/VueAboutProfil.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 import { IonContent,  IonPage } from '@ionic/react';
 
 import Card from '../../../components/RessourceCard';
+import RouteProfil from '../../../components/childProfils/RouteProfil';
+import profilPicture from "../../../img/profilPicture.png";
 
 const VueAboutProfil: React.FC = () => {
   return (
     <IonPage>
       <IonContent>
-          <Card/>
+        <RouteProfil />
+        <section className="marginSection"> 
+          <div className="topAboutProfil">
+            <img src={profilPicture} alt="photo jeune homme" ></img>
+            <h2>Fabien</h2>
+            <p>Développeur informatique</p>
+          </div> 
+            
+        </section>
+         
       </IonContent>
     </IonPage>
   );
--- src/pages/view/viewProfils/VueGallerieProfil.tsx
+++ src/pages/view/viewProfils/VueGallerieProfil.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import { IonContent,  IonPage } from '@ionic/react';
+import mobiscroll from '@mobiscroll/react';
+import '@mobiscroll/react/dist/css/mobiscroll.min.css';
 
 import Card from '../../../components/RessourceCard';
+import RouteProfil from '../../../components/childProfils/RouteProfil';
 
 const VueGallerieProfil: React.FC = () => {
   return (
     <IonPage>
       <IonContent>
-          <Card/>
+        <RouteProfil />
+        <section className="marginSection">          
+          <h3>Gallerie d'images </h3>
+          <mobiscroll.FormGroup>
+            <mobiscroll.ScrollView
+              theme="ios"
+              themeVariant="light"
+              layout={1}
+              paging={true}
+              className="md-fullpage"
+            >
+              {
+                Array(10).fill(null).map((_, i) => (
+                  <Card key={i} />
+                ))
+              }
+            </mobiscroll.ScrollView>
+          </mobiscroll.FormGroup>
+        </section>
+      
       </IonContent>
     </IonPage>
   );
